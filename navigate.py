53c5b4478f8f61f715b13871f2f5ef3cf9f7ce4b
laio <laio@protonmail.com>
2023-05-15 21:06:03 -0700

    the tool is fully operational

--- navigate.py
+++ navigate.py
@@ -1,5 +1,8 @@
 import pymel.core as pm
 import placeholder
+import maya.cmds as cmds
+from random import uniform
+
 def unpackAxis(obj):
     verticesList = getVertexPosition(obj)
     x, y, z = verticesList[0]
@@ -33,7 +36,7 @@ def meshPiece(obj):
 
 def getPosition(obj):
     meshOrTransform = {'transform': pm.objectCenter, 'mesh': meshPiece, 'joint': pm.objectCenter}
-    return meshOrTransform[pm.objectcategory(obj)](obj)
+    return meshOrTransform[pm.objectType(obj)](obj)
 
 def getListPosition(list):
     position = []
@@ -95,7 +98,95 @@ def subDivide(selection, amount):
         placeholder.snap("{}to{}_{}".format(selection[0], selection[-1], a), newPosition)
 
 
+def matchAttributes(source, target, attributeList, axisList):
+    
+    for axis in axisList:
+        for attribute in attributeList:
+            for s, t in zip(source, target):
+                value = t.getAttr("{}{}".format(attribute, axis))
+                s.setAttr("{}{}".format(attribute, axis), value)
 
+def explode(target, minimum, maximum, attributeList, axisList):
+    
+    for axis in axisList:
+        for attribute in attributeList:
+            for t in target:
+                t.setAttr("{}{}".format(attribute, axis), uniform(minimum, maximum))
+
+
+class ConstraintRope:
+    def __init__(self):
+        self.groupManager={}
+
+    def makeGroup(self, knot, direction):
+        grp=cmds.group(n='{}_{}_Grp'.format(knot, direction), em=True)
+        return grp
+
+    def constraintIt(self, linksList, direction):
+        anchor, hood = self.groupManager[direction]
+        weight=0.33
+        for link in range(len(linksList) - 1):
+            pm.parentConstraint(linksList[link], linksList[link + 1], mo=True, w=weight)
+            pm.scaleConstraint(linksList[link], linksList[link + 1], mo=True, w=weight)
+            pm.parentConstraint(anchor, linksList[link], mo=True, w=weight)
+            pm.scaleConstraint(anchor, linksList[link], mo=True, w=weight)
+
+    def chain(self,knotsList, direction):
+        groupList=[]
+        for knot in knotsList:
+            groupList.append(self.makeGroup(knot, direction))
+
+        anchor = self.makeGroup('anchor', direction)
+        hood = self.makeGroup('hood', direction)
+        self.groupManager[direction]= [ anchor, hood]
+
+        pm.parent(anchor, hood)
+        for grp in groupList:
+            pm.parent(grp,hood)
+
+        return groupList
+
+    def tie(self, knotsList):
+
+        driven = self.chain(knotsList, 'driven')
+        forward = self.chain(knotsList, 'forward')
+        backward = self.chain(knotsList, 'backward')
+
+        self.constraintIt(forward, 'forward')
+        backward.reverse()
+        self.constraintIt(backward, 'backward')
+        backward.reverse()
+        weight = 0.33
+        ctrlGrp = self.makeGroup('ctrl', 'ctrl')
+
+        for up, down, drv, knot in zip(forward, backward, driven, knotsList):
+            pm.parentConstraint(up, drv, mo=True, w=0.5)
+            pm.parentConstraint(down, drv, mo=True, w=0.5)
+            pm.scaleConstraint(up, drv, mo=True, w=0.5)
+            pm.scaleConstraint(down, drv, mo=True, w=0.5)
+
+            pm.parent(knot, drv)
+
+            ctrl=pm.circle(nr=(0, 0, 1), c=(0, 0, 0))[0]
+            pm.addAttr(ctrl, ln='offset', k=True, at='float')
+            grp=pm.group(n='{}_offset_grp'.format(knot))
+            pm.parent(grp, ctrlGrp)
+            pm.matchTransform(grp, knot)
+                        
+            UpParent = pm.parentConstraint(ctrl, up, mo=True, w=weight)
+            UpScale = pm.scaleConstraint(ctrl, up, mo=True, w=weight)
+            DownParent = pm.parentConstraint(ctrl, down, mo=True, w=weight)
+            DownScale = pm.scaleConstraint(ctrl, down, mo=True, w=weight)
+            
+            WeightDistribution = lambda node : pm.listAttr(node, k=True)[-1]
+            
+            ctrl.connectAttr('offset','{}.{}'.format(UpParent, WeightDistribution(UpParent)))
+            ctrl.connectAttr('offset','{}.{}'.format(UpScale, WeightDistribution(UpScale)))
+            ctrl.connectAttr('offset','{}.{}'.format(DownParent, WeightDistribution(DownParent)))
+            ctrl.connectAttr('offset','{}.{}'.format(DownScale, WeightDistribution(DownScale)))
+            
+            ctrl.setAttr('offset', 0.34)
+           
 
 
 
--- toolUI.py
+++ toolUI.py
@@ -3,12 +3,14 @@ from PySide2.QtGui import QPalette, QColor
 from PySide2.QtWidgets import QApplication, QMainWindow, QSizePolicy, QWidget, QGridLayout, QPushButton,QCheckBox, QSpinBox
 import navigate
 import pymel.core as pm
-dir(QSpinBox())
+from random import uniform
+    
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
         self.builtLayouts = {}
+        self.loadedSelection = []
 
         self.setWindowTitle("attribute match")
 
@@ -17,38 +19,51 @@ class MainWindow(QMainWindow):
 
         self.chainLayout(layoutInputs, mainLayout)
         
-        self.intBox = QSpinBox()
+        self.stepValue = QSpinBox()
+        self.stepValue.setValue(3)
         self.centerOfSelection = self.button("center of selection")
         self.centerOfSelection.clicked.connect(lambda: navigate.ofAll(pm.ls(os=True, fl=True)))
         
         self.tie = self.button("tie")
-        self.tie.clicked.connect(lambda: navigate.ConstraintRope.tie(pm.ls(os=True, fl=True)))
+        self.tie.clicked.connect(lambda: navigate.ConstraintRope().tie(pm.ls(os=True, fl=True)))
 
         self.centerOfEach = self.button("center of each")
         self.centerOfEach.clicked.connect(lambda: navigate.ofEach(pm.ls(os=True, fl=True)))
         
         self.centerOfStep= self.button("center of step")
-        self.centerOfStep.clicked.connect(lambda: navigate.ofStep(pm.ls(os=True, fl=True), self.intBox.value()))
+        self.centerOfStep.clicked.connect(lambda: navigate.ofStep(pm.ls(os=True, fl=True), self.stepValue.value()))
         
         self.subdivide= self.button("subdivide")        
-        self.subdivide.clicked.connect(lambda : navigate.subDivide(pm.ls(os=True, fl=True), self.intBox.value()))
+        self.subdivide.clicked.connect(lambda : navigate.subDivide(pm.ls(os=True, fl=True), self.stepValue.value()))
+
+        self.maxExplode = QSpinBox()
+        self.minExplode = QSpinBox()
+        self.maxExplode.setValue(25)
+        self.minExplode.setValue(4)
+
+        self.explodeButton= self.button("explode")        
+        self.explodeButton.clicked.connect(lambda : navigate.explode(pm.ls(os=True, fl=True), self.minExplode.value(), self.maxExplode.value(), self.attributeList(), self.axisList()))
         
-        self.loadSelection = self.button("load selection")
-        self.translate = self.button("translate")
-        self.rotate = self.button("rotate")
-        self.scale = self.button("scale")
-        self.allAttributes = self.button("all attributes")
+        self.loadSelectionButton = self.button("load selection")
+        self.loadSelectionButton.setCheckable(True)
+        self.loadSelectionButton.setChecked(False)
+        self.loadSelectionButton.clicked.connect(lambda checked: self.unloadSelection() if not checked else self.loadSelection())
+        
         self.checkBoxX = self.checkBox("X")
         self.checkBoxY = self.checkBox("Y")
-        self.checkBoxZ = self.checkBox("Z")      
-        
-        self.scale.clicked.connect(lambda: pm.ls(os=True, fl=True))
+        self.checkBoxZ = self.checkBox("Z")
+
+        self.checkBoxTranslate = self.checkBox("translate")
+        self.checkBoxRotate = self.checkBox("rotate")
+        self.checkBoxScale = self.checkBox("scale")    
+
+        self.match = self.button("match")
+        self.match.clicked.connect(lambda: navigate.matchAttributes(pm.ls(os=True, fl=True), self.loadedSelection, self.attributeList(), self.axisList()))        
 
         self.builtLayouts['stepLayout'].addWidget(self.subdivide, 0, 1,2,1)
         self.builtLayouts['stepLayout'].addWidget(self.tie, 0, 2,2,1)
-        self.builtLayouts['stepLayout'].addWidget(self.intBox, 0, 0)
-        self.builtLayouts['stepLayout'].addWidget(self.centerOfStep, 1, 0)
-        
+        self.builtLayouts['stepLayout'].addWidget(self.stepValue, 0, 0)
+        self.builtLayouts['stepLayout'].addWidget(self.centerOfStep, 1, 0)        
 
         self.builtLayouts['placeholderLayout'].addWidget(self.centerOfSelection, 1, 0)
         self.builtLayouts['placeholderLayout'].addWidget(self.centerOfEach, 2, 0)
@@ -58,19 +73,56 @@ class MainWindow(QMainWindow):
         self.builtLayouts['axisLayout'].addWidget(self.checkBoxZ,0,2)
 
         
-        self.builtLayouts['attributesLayout'].addWidget(self.loadSelection,0,0, 0,1)
-        self.builtLayouts['attributesLayout'].addWidget(self.translate,0,1)
-        self.builtLayouts['attributesLayout'].addWidget(self.rotate,1,1)
-        self.builtLayouts['attributesLayout'].addWidget(self.scale,2,1)
-        self.builtLayouts['attributesLayout'].addWidget(self.allAttributes,3,1)
+        self.builtLayouts['attributesLayout'].addWidget(self.loadSelectionButton,0,0, 5,1)
+        self.builtLayouts['attributesLayout'].addWidget(self.checkBoxTranslate,0,1)
+        self.builtLayouts['attributesLayout'].addWidget(self.checkBoxRotate,1,1)
+        self.builtLayouts['attributesLayout'].addWidget(self.checkBoxScale,2,1)
+        self.builtLayouts['attributesLayout'].addWidget(self.match,3,1)
+
+
+        self.explodeLayout = QGridLayout()
+        self.builtLayouts['attributesLayout'].addLayout(self.explodeLayout, 4,1)
+        self.explodeLayout.addWidget(self.minExplode,0,0)
+        self.explodeLayout.addWidget(self.maxExplode,0,1)
+        self.explodeLayout.addWidget(self.explodeButton,1,0, 1, 2)
 
         widget = QWidget()
         widget.setLayout( mainLayout )
         self.setCentralWidget(widget)
 
+    def axisList(self):        
+        chosenList=[]
+        if self.checkBoxX.isChecked():
+            chosenList.append('x')
+        if self.checkBoxY.isChecked():
+            chosenList.append('y')
+        if self.checkBoxZ.isChecked():
+            chosenList.append('z')
+
+        return chosenList
+    
+    def attributeList(self):        
+        chosenList=[]
+        if self.checkBoxTranslate.isChecked():
+            chosenList.append('t')
+        if self.checkBoxRotate.isChecked():
+            chosenList.append('r')
+        if self.checkBoxScale.isChecked():
+            chosenList.append('s')
+
+        return chosenList
+
+    def loadSelection(self):
+        for obj in pm.ls(os=True, fl=True):
+            self.loadedSelection.append(obj)
+
+        return self.loadedSelection
+
+    def unloadSelection(self):
+        del self.loadedSelection[:]
 
     @staticmethod
-    def button(name, function=nope, inputs=[]):
+    def button(name):
         pushButton=QPushButton(name)
         pushButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
   
